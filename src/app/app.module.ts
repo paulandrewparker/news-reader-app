674ddb0ddf5c73df0b96976162a5941ec225964d
paulandrewparker <paulandrewparker+1@gmail.com>
2020-03-25 15:51:38 -0400

    1 file modified - debugging issues around news-api.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,30 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { NewsApiService } from './news-api.service';
-
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
 
+import { AppComponent } from './app.component';
+
 @NgModule({
-  declarations: [ AppComponent, HelloComponent ],
-  imports:      [ BrowserModule, FormsModule, BrowserModule, BrowserAnimationsModule, HttpClientModule, MatButtonModule, MatMenuModule, MatCardModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, 
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
   ],
   providers: [NewsApiService],
-  bootstrap: [AppComponent],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
